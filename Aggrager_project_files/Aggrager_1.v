dcf09a9ec331d0efb68b1744cafd3188cff4eb5f
sheild21 <52274858+sheild21@users.noreply.github.com>
2019-07-29 13:46:43 +0530

    code-4 with input file

--- Aggrager_project_files/Aggrager_1.v
+++ Aggrager_project_files/Aggrager_1.v
@@ -42,14 +42,16 @@ module Aggregator #(
 //---------------------------------------------------------------------------------------------------------------------
 // parameter definitions
 //---------------------------------------------------------------------------------------------------------------------
-	parameter 		DATA_WIDTH		=	12'h0FF, //255
-	parameter 		LENGTH_WIDTH	=	8'h1F    //31
+	parameter 		DATA_WIDTH		=	255, //255
+	parameter 		LENGTH_WIDTH	=	31    //31
 )(	
 	clk,
 	reset,
 	start,
 	DATA_IN1,
 	DATA_IN2,
+	valid_1,
+	valid_2,
 	ready,
 	ready_1,
 	ready_2,
@@ -68,6 +70,8 @@ module Aggregator #(
 	input								start 		;
 	input			[DATA_WIDTH:0]		DATA_IN1	;
 	input			[DATA_WIDTH:0]		DATA_IN2	;
+	input                               valid_1     ;
+	input                               valid_2     ;
 	input 								ready 		;
 	output	reg							ready_1 	;
 	output	reg							ready_2 	;
@@ -84,33 +88,50 @@ module Aggregator #(
 //---------------------------------------------------------------------------------------------------------------------
 
 	localparam CHECK_START			=	4'h0;	
-	localparam READY1				=	4'h1;
-	localparam READY2				=	4'h2;
-	localparam GET_DATA1			=	4'h3;
-	localparam GET_DATA2			=	4'h4;
-	localparam CHECK_LENGTH			=	4'h5;
-	localparam CHECK_ROW			=	4'h6;
-	localparam OUTPUT				=	4'h7;
-	localparam NEXT_DATA			=	4'h8;
-	localparam WAIT					=	4'h9;
+	localparam READY				=	4'h1;
+	localparam GET_DATA				=	4'h2;
+	localparam LENGTH_BEGIN			=	4'h3;
+	localparam GET_LENGTH			=	4'h4;
+	localparam COMBINE				=	4'h5;
+	localparam CHECK_LENGTH1		=	4'h6;
+	localparam CHECK_LENGTH2		=	4'h7;
+	localparam CHECK_LENGTH3		=	4'h8;
+	localparam BLOCK_UPDATE			=	4'h9;
+	localparam BUFFER				=	4'hA;
+	localparam OUTPUT				=	4'hB;
+	localparam NEW_LENGTH			=	4'hC;
+	localparam NEXT_DATA			=	4'hD;
+	localparam COUNT       			=	4'hE;
+	localparam READY_LOW       		=	4'hF;
+	
 
 	localparam ENGINE1				=	1'b0;
 	localparam ENGINE2				=	1'b1;
+
+	localparam stage1				=	2'b00;
+	localparam stage2				=	2'b01;
+	localparam stage3				=	2'b10;
+	localparam stage4				=	2'b11;
 
 
 //---------------------------------------------------------------------------------------------------------------------
 // Internal wires and registers
 //---------------------------------------------------------------------------------------------------------------------
 
-	reg 			[DATA_WIDTH:0] 		DATA 		;
+	reg 			[DATA_WIDTH:0] 		data 		;
+	reg 			[DATA_WIDTH:0] 		data_buffer ;
 	reg 			[3:0]				state 		;
 	reg 			[3:0]				next_state	;
 	reg 								next_engine	;
 	reg             [LENGTH_WIDTH:0]	count       ;
 	reg                                 count_done  ;
-	reg 			[LENGTH_WIDTH:0]	LENGTH 		;
-	reg 			[LENGTH_WIDTH:0]	ROW 		;
-	reg 			[LENGTH_WIDTH:0]	ROW_NUM		;		
+	reg							        flag_1 	    ;
+    reg                                 flag_2      ;
+	reg 			[LENGTH_WIDTH:0]	length 		;
+	reg 			[LENGTH_WIDTH:0]	past_len	;
+	reg 			[1:0]				stage		;
+	reg 			[LENGTH_WIDTH:0]	block_num	;	
+	reg 			[LENGTH_WIDTH:0]	block 		;		
 
 
 
@@ -128,49 +149,93 @@ module Aggregator #(
 
 		    	CHECK_START		:	begin
 			    						if (start) begin
-			    							next_state=READY1;
+			    							next_state=READY;
 			    						end else begin
 			    							next_state=CHECK_START;
 			    						end
 			    					end
+			    READY 			:	next_state	=	READY_LOW;
+			    
+			    READY_LOW       :   next_state	=	COUNT;
+
+			    COUNT 			:   begin
+			    						if (count_done) begin
+			    							next_state	=	GET_DATA;
+			    						end else begin
+			    							next_state	=	COUNT;
+			    						end
+			    					end
 
 
-			   	READY1			:	next_state	=	GET_DATA1;
-				   						
-				READY2   		:	next_state	=	GET_DATA2;
-				   				
+			    GET_DATA 		:	begin
+                                         if (((flag_1)&& (valid_1))||((flag_2)&& (valid_2))) begin
+                                            case(stage)
+                                            stage1	:	next_state	=	LENGTH_BEGIN;
+                                            stage2	:	next_state	=	OUTPUT;
+                                            stage3	:	next_state	=	CHECK_LENGTH2;
+                                            stage4	:	next_state	=	COMBINE;
+                                            endcase
+                                         end else begin
+                                            next_state	=	GET_DATA;	
+                                         end
+			    						
+			    					end
+                
 
-			   	GET_DATA1		:	next_state	=	CHECK_LENGTH;
+			    
 
-			   	GET_DATA2		:	next_state	=	OUTPUT;
+			    LENGTH_BEGIN	:	next_state	=	CHECK_LENGTH1;
 
-			   	CHECK_LENGTH	:	next_state	=	CHECK_ROW;
+			    COMBINE			:	next_state	=	GET_LENGTH;
 
-			   	CHECK_ROW		:	next_state	=	OUTPUT;
+			    GET_LENGTH		:	next_state	=	CHECK_LENGTH3;
+
+			   		   	
+			   
+			   	CHECK_LENGTH1	:	begin
+			   							if (length>=32) begin
+			   								next_state	=	OUTPUT;
+			   							end else begin
+			   								next_state	= 	BLOCK_UPDATE;
+			   								
+			   							end
+			   						end
+
+
+		   		CHECK_LENGTH2	:	begin
+		   								next_state	= 	BLOCK_UPDATE;
+		   							
+		   							end
+
+		   					   	
+			   	CHECK_LENGTH3	:	begin
+			   							if (length>=32-(block<<2)) begin
+			   								next_state	=	OUTPUT;
+			   							end else begin
+			   								next_state	= 	BLOCK_UPDATE;
+
+			   							end
+			   						end
+			   	BLOCK_UPDATE	:	next_state	= 	BUFFER;
+
+			   	BUFFER			:	next_state = READY;
 
 			   	OUTPUT 			:	begin
 			   							if (ready) begin
-			   								next_state	=	NEXT_DATA;
+			   								next_state	=	NEW_LENGTH;
 			   							end else begin
 			   								next_state	=	OUTPUT;
 			   							end
 			   						end
 
+			   	NEW_LENGTH		:	next_state	=	NEXT_DATA;
+
 			   	NEXT_DATA		:	begin
-			   							if (LENGTH>8) begin
-			   								next_state	=	WAIT;
-			   							end else begin
-			   								next_state	=	READY1;
-			   							end
+			   							
+			   							next_state	=	READY;
 			   						end
 
-			   	WAIT			:   begin
-				   						if (count_done) begin
-				   							next_state	=	READY1;
-				   						end else begin
-				   							next_state	=	READY2;
-				   						end
-				   					end
+			   	
 			   						
 
 
@@ -181,92 +246,182 @@ module Aggregator #(
 	always@(posedge clk) begin
 	    if(reset) begin
 	        next_engine	<= 1'b0;
+	        data_buffer	<=	0;
+	        block_num <= 0;
+	        past_len  <= 0;
+	        stage <= stage1;
+	        block <= 0;
+	        ready_1 <=0;
+	        ready_2 <= 0;
+	        data <=0;
+	        
+	        
 	    end else begin
 	        case(state)
+	        	
 
-		    	READY1,READY2	:	begin											//SENDING THE READY SIGNALS
+	        	
+
+		    	READY			:	begin											//SENDING THE READY SIGNALS
 			   							case(next_engine)
 			   								ENGINE1	:	begin
 			   												ready_1<=1'b1;
+			   												flag_1 <=1'b1;
 			   											end
 			   								ENGINE2 :	begin 
 			   												ready_2<=1'b1;
+			   												flag_2 <=1'b1;
 			   											end	
 			   							endcase
 			   						end
-
-		    	GET_DATA1,GET_DATA1	:begin											//
+			   	
+			   	READY_LOW       :    begin   
+                                         ready_1 <=0;
+                                         ready_2 <=0;
+			   	                     end
+		    	GET_DATA 		:	begin											//
 			   							case(next_engine)
 			   								ENGINE1	:	begin
-			   												DATA<=DATA_IN1;
-			   												valid<=1'b1;
+			   								              if ((flag_1) && (valid_1)) begin
+			   												data<=DATA_IN1;
+			   											  end 
+			   											    
+			   												
 			   											end
 			   								ENGINE2 :	begin 
-			   												DATA<=DATA_IN2;
-			   												valid<=1'b1;
+			   											  if ((flag_2) && (valid_2)) begin
+                                                                data<=DATA_IN2;
+                                                          end
+			   												
 			   											end	
 			   							endcase
 			   						end
-			   	CHECK_LENGTH	:	begin
-                                        LENGTH 	<=	DATA[LENGTH_WIDTH:0];
-                                        ROW 	<=	(LENGTH >> 3);
+			   	LENGTH_BEGIN	: 	begin
+                                        length <= data[LENGTH_WIDTH:0] ;
+                                        flag_1 <=0;
+                                        flag_2 <=0;
+			   	                    end
+			   	COMBINE			:	begin
+			   							data<=data| data_buffer;
+			   							flag_1 <=0;
+                                        flag_2 <=0;
 			   						end
 
-			   	CHECK_ROW		:	ROW_NUM	<=	ROW +1'b1;							//NO OF ROWS FOR THE LENGTH
-			   						//FULL_ROW_LENGTH <= (ROW<<3);
-			   						//DATA_REMAIN<=LENGTH -
+			   	GET_LENGTH		:	begin
+			   	                         case(block)
+			   	                         1:length 	<=	data[LENGTH_WIDTH+(1<<5):(1<<5)];
+			   	                         2:length 	<=	data[LENGTH_WIDTH+(2<<5):(2<<5)];
+			   	                         3:length 	<=	data[LENGTH_WIDTH+(3<<5):(3<<5)];
+			   	                         4:length 	<=	data[LENGTH_WIDTH+(4<<5):(4<<5)];
+			   	                         5:length 	<=	data[LENGTH_WIDTH+(5<<5):(5<<5)];
+			   	                         6:length 	<=	data[LENGTH_WIDTH+(6<<5):(6<<5)];
+			   	                         7:length 	<=	data[LENGTH_WIDTH+(7<<5):(7<<5)];
+                                            
+                                        endcase
+			   						end
+
+			
 
 			   	OUTPUT 			:	begin
+			   	                        flag_1 <=0;
+                                        flag_2 <=0;
 			   							if (ready) begin
-			   								DATA_OUT<=	DATA;
+			   								DATA_OUT<=	data;
+			   								valid 	<=  1;	
 			   							end 
 			   						end
 
-			   	NEXT_DATA		:	begin
-			   							if (LENGTH<=8) begin
-			   								next_engine	<= ~next_engine;
+			   	CHECK_LENGTH1	:	begin
+			   							if (length<32) begin
+			   							
+			   								past_len <= length;
+				   							if ((length%4)==0) begin	   								
+				   								block_num <= (length>>2);
+				   							end else begin
+				   								block_num <= (length>>2)+1;
+				   							end
 			   							end
 			   						end
 
-			   	WAIT			:	begin
-				   						if (count_done) begin
-				   							next_engine	<= ~next_engine;
-				   						end
-				   					end
+			   	CHECK_LENGTH2	:	begin
+			   							past_len <= length;
+			   							flag_1 <=0;
+                                        flag_2 <=0;
+			   							if ((length%4)==0) begin	   								
+			   								block_num <= (length>>2);
+			   							end else begin
+			   								block_num <= (length>>2)+1;
+			   							end
+			   						end
+
+			   	CHECK_LENGTH3	:	begin
+			   							if (length<32-(block<<2)) begin
+				   							past_len <= length;
+				   							if ((length%4)==0) begin	   								
+				   								block_num <= (length>>2);
+				   							end else begin
+				   								block_num <= (length>>2)+1;
+				   							end
+			   							end
+			   						end
+			   	BLOCK_UPDATE	:	block <= block+block_num;
+
+			   	
+
+			   	BUFFER			:	begin
+			   							data_buffer<=data;
+			   							stage <= stage4;
+			   							next_engine <= ~next_engine;
+
+			   						end
+			   	NEW_LENGTH		:	begin
+			   							length <= length-(32-(block<<2));					
+			   						end
+
+			   	NEXT_DATA		:	begin
+			   							block <=0;
+			   							if (length==0) begin
+			   								stage<=	stage1;
+			   								next_engine <= ~next_engine;
+			   							end else if (length>32) begin
+			   								stage<=	stage2;
+			   							end else begin
+			   								stage<=	stage3;
+
+			   						    
+			   							
+			   							end	
+			   						end
+
 
 		    endcase // state
 	    end
     end
 
+    always @(posedge clk) begin
+    	if (reset) begin
+    		count <=0;
+    	end else begin
+    		case (state)
+    			COUNT 			:   begin
+			    						if (count_done) begin
+			    							count <=0;
+			    						end else begin
+			    							count 	<= count + 1 ;
+			    						end			    						
+			    					end
 
-	always @(posedge clk) begin
-	    if(reset) begin
-	       count     <= 'b1;
-	    end else begin 
-	        case(state) 
-		        NEXT_DATA: 	begin
-		        				if (LENGTH>8) begin   
-					               if(count_done) begin
-					                   count <= 'b1;
-					               end else begin
-					                   count <= count + 1'b1;
-					               end
-				            	end
-				            end
-	        endcase
-	    end
+			endcase
+		end
 	end
-   
-    always @(*) begin
-        count_done = 1'b0;
-        case(state) 
-        NEXT_DATA	:count_done = (count == ROW_NUM);
-        WAIT 		:count_done = (count == ROW_NUM);
-        
-       
-      
-       endcase
-    end 
 
-
+	always @(*) begin
+		count_done = 0;
+		case(state)
+			COUNT 				:   begin
+										count_done=(count==5);
+									end
+		endcase
+	end
 endmodule // Aggregator
+ 
--- Aggrager_project_files/Aggrager_1_Test.v
+++ Aggrager_project_files/Aggrager_1_Test.v
@@ -39,31 +39,52 @@
 
 module Aggregator_test ;
 
-	reg								clk			;
-	reg								reset 		;
-	reg								start 		;
-	reg			[DATA_WIDTH-1:0]	DATA_IN1	;
-	reg			[DATA_WIDTH-1:0]	DATA_IN2	;
-	reg 							ready 		;
-	wire							ready_1 	;
-	wire							ready_2 	;
-	wire		[DATA_WIDTH:0]		DATA_OUT 	;
-	wire 							valid		;
+	parameter 		DATA_WIDTH		=	255; 
+	parameter 		LENGTH_WIDTH	=	31 ;  
+	parameter 		INPUT_ENGINE1	=	"G:/intern/Aggrager_project_files/ENGINE_1_OUT.txt";
+	parameter 		INPUT_ENGINE2	=	"G:/intern/Aggrager_project_files/ENGINE_2_OUT.txt";
+	parameter		OPEN_FIRST_FILE	=	1;
+	parameter		OPEN_FILE2     	=	2;
+	parameter		OPEN_FILE1		=	3;
+	parameter       CHECK_READY     =   4;
+	parameter		SEND_FILE1		=	5;
+	parameter 		SEND_FILE2		=	6;
+	parameter 		CHECK_ENGINE	=	7;
+	
 
-	module Aggregator #(
+	reg									clk			;
+	reg									reset 		;
+	reg									start 		;
+	reg			        [DATA_WIDTH:0] 	DATA_IN1	;
+	reg			        [DATA_WIDTH:0]  DATA_IN2	;
+	reg                             	valid_1     ;
+	reg                             	valid_2     ;
+	reg 								ready 		;
+	wire								ready_1 	;
+	wire								ready_2 	;
+	wire		        [DATA_WIDTH:0]	DATA_OUT 	;
+	wire 								valid		;
+
+	reg					[2:0]			state1		;
+	reg                                 set         ;
+	reg			        [DATA_WIDTH:0] 	input_data	;
+	
+	integer input_engine1,input_engine2,r;
+
+Aggregator #(
 		12'h0FF,
 		8'h1F
-	)(	
+	) Aggregator1(	
 	.clk(clk),
 	.reset(reset),
 	.start(start),
+	.valid_1(valid_1),
+	.valid_2(valid_2),
 	.DATA_IN1(DATA_IN1)	,
 	.DATA_IN2(DATA_IN2),
-	.DATA_OUT(DATA_OUT),
-	.valid(valid),
 	.ready(ready)	,
 	.ready_1 (ready_1),
-	.ready_2(ready_2,
+	.ready_2(ready_2),
 	.DATA_OUT(DATA_OUT),
 	.valid(valid)
 
@@ -78,7 +99,117 @@ module Aggregator_test ;
     end
 
 	initial begin
-		reset <=
+		reset <=1'b1;
+		repeat(15) @(posedge clk);
+		reset <= 1'b0;
+		start <=1'b1;
+		repeat(1) @(posedge clk);
+		start <= 1'b0;
+		valid_1 <= 1;
+		set	<= 1;
+		//repeat(10) @(posedge clk);
+		
+		//valid_1 <= 0;
+		//valid_2 <= 1;iiiii
+		ready <=1'b1;
+		
+		/*DATA_IN1<=256'h0000000000ffff07ffff7f0080ffff1fffffffffc1ffff1f00e0ffff00000020;
+		repeat(10) @(posedge clk);
+		DATA_IN1<=256'h00000000000000000000000000000000000000000000000000000000ff7f0080;
+		DATA_IN2<=256'hcffff01000ffff07ffff7f0080ffff1fff0100feff7ffcff0000002000000020;
+		repeat(15) @(posedge clk);
+		DATA_IN1<=256'h7ff0ffff0700f8fffff1ffffff1ffcffff0100feff7ffcffffff07ffff7f0080;
+		DATA_IN2<=256'h7ff0ffff0700f8fffff1ffffff1ffcffff0100feff7ffcffffff07ffff7f0080;*/
+		
+	end
+
+		always @(posedge clk) begin
+			 if(reset) begin
+	            state1   	<= OPEN_FIRST_FILE;
+	        end else begin
+	            case(state1)   	
+	            	OPEN_FIRST_FILE 	:	begin
+	            								input_engine1=$fopen(INPUT_ENGINE1,"r");
+	            								state1	<=  CHECK_READY;
+	            							end
+	            	OPEN_FILE2		 	:	begin
+	            								input_engine2=$fopen(INPUT_ENGINE2,"r");
+	            								state1	<=  SEND_FILE2;
+	            							end
+		            OPEN_FILE1 			: 	begin
+							                    /*if(io_out_tlast && io_out_ready && io_out_valid) begin
+							                        input_engine1 = $fopen(INPUT_ENGINE1,"r");
+							                        state1 <= SEND_FILE1;
+							                        set <= 1; 
+							                    end*/
+						                	end	
+				    CHECK_READY         :   begin
+				                                if (ready_1)begin
+				                                    state1	<=  SEND_FILE1;
+				                                end else begin
+				                                    state1	<=  CHECK_READY;
+				                                end
+				                            end					
+	            	SEND_FILE1			:	begin
+	            								if (!$feof(input_engine1)) begin
+	            									if (valid_1) begin
+	            										r= $fscanf(input_engine1,"%h\n",input_data);
+	            										DATA_IN1<=input_data;
+	            										state1 <= CHECK_ENGINE;	
+	            									end else begin
+	            										valid_1 <=1;
+	            										r= $fscanf(input_engine1,"%h\n",input_data);
+	            										DATA_IN1<=input_data;
+	            										state1 <= CHECK_ENGINE;
+	            									end
+	            								end else begin
+							                        if(ready_1) begin
+							                            valid_1     <= 0;
+							                            $fclose(input_engine1);
+							                            state1 <= OPEN_FILE1;
+							                        end
+							                    end
+							                end
+					SEND_FILE2			:	begin
+	            								if (!$feof(input_engine2)) begin
+	            										valid_2 <=1;
+	            										r= $fscanf(input_engine2,"%h\n",input_data);
+	            										DATA_IN2<=input_data;
+	            										state1 <= CHECK_ENGINE;
+	            								end else begin
+							                        if(ready_2) begin
+							                            valid_2     <= 0;
+							                            $fclose(input_engine1);
+							                            state1 <= OPEN_FILE1;
+							                        end
+							                    end
+							                 end
+					CHECK_ENGINE		:	begin	
+												if (ready_1) begin
+													state1 <= SEND_FILE1;
+												end else if (ready_2)begin
+													if (set) begin
+														state1 <= OPEN_FILE2 ;
+														set <= 0;
+													end else begin
+														state1 <= SEND_FILE2;
+													end
+
+												end else begin
+													state1 <= CHECK_ENGINE ;
+												end
+											end
+				/*	default				:	begin
+										 	end*/
+
+		            endcase        	
+
+	        end
+
+
+		end
+
+
 
 
 
